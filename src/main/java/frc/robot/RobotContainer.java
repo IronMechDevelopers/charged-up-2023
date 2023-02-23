28ab83f72953e685a764839953be533ffcad5f11
nathyeli.delgado <nathyelidelgado@trentoncatholicprep.org>
2023-02-23 15:06:57 -0500

    using xbox

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,6 +19,8 @@ import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Wrist;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -44,6 +46,7 @@ public class RobotContainer {
   private final Joystick driverLeftStick = new Joystick(0);
   private final Joystick driverRightStick = new Joystick(1);
   private final Joystick copilot = new Joystick(2);
+  XboxController copilotXbox = new XboxController(3); 
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -74,66 +77,75 @@ public class RobotContainer {
    */
   private void configureBindings() {
 
-    // final JoystickButton balanceButton = new JoystickButton(driverLeftStick, 3);
+     //final JoystickButton balanceButton = new JoystickButton(driverLeftStick, 12);
 
     // final JoystickButton armDownButton = new JoystickButton(driverRightStick, 7);
-    // final JoystickButton armUpButton = new JoystickButton(driverRightStick, 8);
-
-    final JoystickButton rightFire = new JoystickButton(driverRightStick, 1);
-    final JoystickButton leftFire = new JoystickButton(driverLeftStick, 1);
-
-    final JoystickButton armLowButton = new JoystickButton(driverLeftStick, 7);
-    final JoystickButton armHighButton = new JoystickButton(driverLeftStick, 8);
-
-    final JoystickButton oneEighty = new JoystickButton(driverRightStick, 10);
-
-    final JoystickButton intakeInButton = new JoystickButton(driverRightStick, 5);
-    final JoystickButton intakeOutButton = new JoystickButton(driverRightStick, 6);
-
-    final JoystickButton wristDownButton = new JoystickButton(driverRightStick, 3);
-    final JoystickButton wristUpButton = new JoystickButton(driverRightStick, 4);
+    // final JoystickButton armUpButton = new JoystickButton(driverRightStick, 8); 
 
 
-    // final JoystickButton driveForwardDistanceButton2 = new JoystickButton(driverRightStick, 5);
-
-
-    // final JoystickButton driveForwardDistanceButton = new JoystickButton(driverLeftStick, 5);
-
-    // final JoystickButton angleCorrecter = new JoystickButton(driverLeftStick, 6);
-
-    // final JoystickButton driveStraight = new JoystickButton(driverRightStick, 11);
-
-    oneEighty.toggleOnTrue(new Angle(m_drivetrain, 180));
-
-    // balanceButton.whileTrue(new Balance(m_drivetrain));
-
-    armLowButton.toggleOnTrue(new MoveArmToDistance(m_arm, 0, 1));
-    armHighButton.toggleOnTrue(new MoveArmToDistance(m_arm, 6, 1));
-
-    // armDownButton.whileTrue(new MoveArm(m_arm, -1, 1));
-    // armUpButton.whileTrue(new MoveArm(m_arm, 1, 1));
-
-
-    intakeInButton.whileTrue(new Collection(m_intake, 1, .75));
-    intakeOutButton.whileTrue(new Collection(m_intake, -1, .75));
-
-    leftFire.whileTrue(new MoveArm(m_arm,1,1));
-    rightFire.whileTrue(new MoveArm(m_arm,-1,.75));
-
-    wristDownButton.whileTrue(new MoveWrist(m_wrist, -1, .5));
-    wristUpButton.whileTrue(new MoveWrist(m_wrist, 1, .5));
+    //final JoystickButton AutoBalance= new JoystickButton(driverRightStick, 11);
 
     
-    // driveStraight.toggleOnTrue(new AutoBalance(m_drivetrain));
+
+    new JoystickButton(copilotXbox, Button.kLeftBumper.value)
+    .whileTrue(new Collection(m_intake, -1, .75));
+
+    new JoystickButton(copilotXbox, Button.kRightBumper.value)
+    .whileTrue(new Collection(m_intake, 1, .75));
+
+    new JoystickButton(copilotXbox, Button.kA.value)
+    .whileTrue(new MoveWrist(m_wrist, 1, .5));
+
+    new JoystickButton(copilotXbox, Button.kB.value)
+    .whileTrue(new MoveWrist(m_wrist, -1, .5));
+
+    new JoystickButton(copilotXbox, Button.kY.value)
+    .whileTrue(new MoveArm(m_arm,1,1));
+
+    new JoystickButton(copilotXbox, Button.kX.value)
+    .whileTrue(new MoveArm(m_arm,-1,.75));
+  
+
+    // // final JoystickButton driveForwardDistanceButton2 = new JoystickButton(driverRightStick, 5);
+
+
+    // // final JoystickButton driveForwardDistanceButton = new JoystickButton(driverLeftStick, 5);
+
+    // // final JoystickButton angleCorrecter = new JoystickButton(driverLeftStick, 6);
+
+    // // final JoystickButton driveStraight = new JoystickButton(driverRightStick, 11);
+
+    // oneEighty.toggleOnTrue(new Angle(m_drivetrain, 180));
+
+    // // balanceButton.whileTrue(new Balance(m_drivetrain));
+
+    // armLowButton.toggleOnTrue(new MoveArmToDistance(m_arm, 0, 1));
+    // armHighButton.toggleOnTrue(new MoveArmToDistance(m_arm, 6, 1));
+
+    // // armDownButton.whileTrue(new MoveArm(m_arm, -1, 1));
+    // // armUpButton.whileTrue(new MoveArm(m_arm, 1, 1));
+
+
+    // intakeInButton.whileTrue(new Collection(m_intake, 1, .75));
+    // intakeOutButton.whileTrue(new Collection(m_intake, -1, .75));
+
+    // leftFire.whileTrue(new MoveArm(m_arm,1,1));
+    // rightFire.whileTrue(new MoveArm(m_arm,-1,.75));
+
+    // wristDownButton.whileTrue(new MoveWrist(m_wrist, -1, .5));
+    // wristUpButton.whileTrue(new MoveWrist(m_wrist, 1, .5));
 
     
-    // angleCorrecter.toggleOnTrue(new TurnToAngle(m_drivetrain, m_drivetrain.getGoalAngle(90)));
+    // // driveStraight.toggleOnTrue(new AutoBalance(m_drivetrain));
 
     
-    // driveForwardDistanceButton.toggleOnTrue(new DriveForwardDistance(m_drivetrain, -36));
+    // // angleCorrecter.toggleOnTrue(new TurnToAngle(m_drivetrain, m_drivetrain.getGoalAngle(90)));
+
+    
+    // // driveForwardDistanceButton.toggleOnTrue(new DriveForwardDistance(m_drivetrain, -36));
 
    
-    // driveForwardDistanceButton2.toggleOnTrue(new DriveForwardDistance(m_drivetrain, -6));
+    // // driveForwardDistanceButton2.toggleOnTrue(new DriveForwardDistance(m_drivetrain, -6));
   }
 
   /**
